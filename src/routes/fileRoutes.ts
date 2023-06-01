00717a9db4ba12c0d0874a7aeb56923afc54cc48
Isaac Smith <103906316+ismith916@users.noreply.github.com>
2023-06-01 16:27:45 -0700

    Updates to delete all files functionality

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -1,15 +1,22 @@
-const express = require('express')
-import { createFile, getFiles, getOneFile, editFile, deleteFile, downloadOneFile } from "../controllers/fileControllers";
-const multer = require('multer')
-const upload = multer({ dest: 'userFiles/' })
+const express = require("express");
+import {
+   createFile,
+   getFiles,
+   getOneFile,
+   editFile,
+   deleteFile,
+   downloadOneFile,
+} from "../controllers/fileControllers";
+const multer = require("multer");
+const upload = multer({ dest: "userFiles/" });
 
-const apps = express()
+const apps = express();
 
-apps.get('/', getFiles);
-apps.post('/', upload.single('file'), createFile);
-apps.get('/:fileId', getOneFile);
-apps.get('/download/:fileId', downloadOneFile);
-apps.put('/:fileId', editFile);
-apps.delete('/:fileId', deleteFile)
+apps.get("/:fileId", getOneFile);
+apps.get("/download/:fileId", downloadOneFile);
+apps.put("/:fileId", editFile);
+apps.delete("/:fileId", deleteFile);
+apps.get("/", getFiles);
+apps.post("/", upload.single("file"), createFile);
 
-export default apps; 
+export default apps;
